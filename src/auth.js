0b5b7277d005637b5bb91a10d56080678b3e1610
Enieber Cunha <eniebercunha@gmail.com>
2018-12-10 19:05:47 -0300

    fix(enieber) run prettier

--- src/auth.js
+++ src/auth.js
@@ -6,25 +6,24 @@ import {
 } from './token';
 
 export const login = (user, password, redirect) => {
-    if (user == 'admin@eusouelliot.com' &&  password == '1234') {
-	const token = generateToken();
-	saveToken(token);	    
-	setTimeout(redirect, 200);
-	return;
-    }
-    alert('Usuario/Senha incorreto');
+  if (user == 'admin@eusouelliot.com' && password == '1234') {
+    const token = generateToken();
+    saveToken(token);
+    setTimeout(redirect, 200);
+    return;
+  }
+  alert('Usuario/Senha incorreto');
 };
 
 export const logout = (redirect) => {
-    setTimeout(redirect, 200);
-    removeToken();
+  setTimeout(redirect, 200);
+  removeToken();
 };
 
 export const hasAuthentication = () => {
-    const token = getToken();
-    if (token) {
-	return token.length > 1;
-    }
-    return false;
+  const token = getToken();
+  if (token) {
+    return token.length > 1;
+  }
+  return false;
 };
-
